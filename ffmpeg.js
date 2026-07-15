fd3c903ed39d21f2f1303dc0204faa49ec4f5156
onfarooq0008-eng <onfarooq0008@gmail.com>
2026-07-15 22:53:45 +0400

    Update ffmpeg.js

--- ffmpeg.js
+++ ffmpeg.js
@@ -1,28 +1,173 @@
-const {spawn}=require('child_process');
-let proc=null;
+const {spawn}=require("child_process");
+const monitor=require("./monitor");
+
+
+let process=null;
 let current=null;
 
+
 function startStream(video,rtmp){
- if(proc) return {status:'already running'};
- current={video,rtmp};
- run();
- return {status:'started'};
+
+
+if(process)
+return {
+status:"already running"
+};
+
+
+current={
+video,
+rtmp
+};
+
+
+run();
+
+
+return {
+status:"started"
+};
+
 }
+
+
 
 function run(){
- proc=spawn('ffmpeg',['-re','-stream_loop','-1','-i',current.video,'-c:v','libx264','-preset','veryfast','-threads','1','-b:v','1200k','-c:a','aac','-f','flv',current.rtmp]);
- proc.on('close',()=>{
-  proc=null;
-  if(current) setTimeout(run,5000);
- });
+
+
+monitor.start();
+
+
+
+process=spawn(
+
+"ffmpeg",
+
+[
+
+"-re",
+
+"-stream_loop",
+"-1",
+
+"-i",
+current.video,
+
+
+"-c:v",
+"libx264",
+
+"-preset",
+"veryfast",
+
+"-threads",
+"1",
+
+"-b:v",
+"1200k",
+
+"-maxrate",
+"1200k",
+
+"-bufsize",
+"2400k",
+
+
+"-c:a",
+"aac",
+
+"-f",
+"flv",
+
+current.rtmp
+
+
+]
+
+);
+
+
+
+process.stderr.on(
+"data",
+data=>{
+
+
+monitor.update(data);
+
+
+console.log(
+data.toString()
+);
+
+
+});
+
+
+
+
+process.on(
+"close",
+()=>{
+
+
+process=null;
+
+
+// automatic recovery
+
+
+if(current){
+
+setTimeout(()=>{
+
+run();
+
+},5000);
+
+
 }
+
+
+});
+
+
+}
+
+
 
 function stopStream(){
- if(proc) proc.kill();
- proc=null;
- current=null;
- return {status:'stopped'};
+
+
+if(process)
+process.kill();
+
+
+process=null;
+current=null;
+
+
+return {
+status:"stopped"
+};
+
+
 }
 
-function status(){return {running:!!proc};}
-module.exports={startStream,stopStream,status};
+
+
+function status(){
+
+return{
+running:!!process
+};
+
+}
+
+
+
+module.exports={
+startStream,
+stopStream,
+status
+};
