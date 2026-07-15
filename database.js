e37a11d3f8bc34a805dcd77609395e5292040198
onfarooq0008-eng <onfarooq0008@gmail.com>
2026-07-15 22:46:51 +0400

    Update database.js

--- database.js
+++ database.js
@@ -1,6 +1,36 @@
 const sqlite3=require('sqlite3').verbose();
 const db=new sqlite3.Database('./database/restreamer.db');
 
+db.run(`
+CREATE TABLE IF NOT EXISTS channels(
+id INTEGER PRIMARY KEY,
+name TEXT,
+platform TEXT,
+url TEXT,
+key TEXT
+)
+`);
+
+
+db.run(`
+CREATE TABLE IF NOT EXISTS videos(
+id INTEGER PRIMARY KEY,
+name TEXT,
+path TEXT,
+thumbnail TEXT
+)
+`);
+
+
+db.run(`
+CREATE TABLE IF NOT EXISTS settings(
+id INTEGER PRIMARY KEY,
+music TEXT,
+logo TEXT,
+volume INTEGER
+)
+`);
+
 db.serialize(()=>{
 db.run('CREATE TABLE IF NOT EXISTS videos(id INTEGER PRIMARY KEY,name TEXT,path TEXT)');
 db.run('CREATE TABLE IF NOT EXISTS channels(id INTEGER PRIMARY KEY,name TEXT,url TEXT,key TEXT)');
